a4f8ad6bc1c3d6bd9d783343492d30d35c258e3c
Agney <agney.menon@bigbinary.com>
2020-10-03 01:53:08 +0530

    introduce ball triangle

--- example/index.tsx
+++ example/index.tsx
@@ -1,11 +1,11 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { useLoading, Audio } from '../.';
+import { useLoading, BallTriangle } from '../.';
 
 const App = () => {
   const { containerProps, indicatorEl } = useLoading({
-    indicator: <Audio />,
+    indicator: <BallTriangle />,
     loading: true,
   });
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export { useLoading } from './useLoading';
 
 // Loaders
 export { Audio } from './loaders/Audio';
+export { BallTriangle } from './loaders/BallTriangle';
